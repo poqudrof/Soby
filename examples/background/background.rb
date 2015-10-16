3b99644fe7a7c1b1b42c810bd92a0aae5583a4e5
Jeremy Laviole <jeremy.laviole@inria.fr>
2015-10-16 14:29:09 +0200

    Added examples, cleaning.

---
+++ examples/background/background.rb
@@ -0,0 +1,31 @@
+## re-open the class
+
+class SobyPlayer
+
+  ## To be overriden by the Presentation Code.
+  def custom_setup
+    @background_image = loadImage sketchPath + "/data/abstract-bubbles.jpg"
+    @font = loadFont sketchPath + "/data/LinLibertine-30.vlw"
+
+    @footer = "Monday, April 1st. -- Important Talk about pranks."
+
+    @setup_done = true
+  end
+
+  def custom_pre_draw
+    return unless @setup_done
+    image(@background_image, 0, 0)
+
+    fill(255, 120)
+    textFont(@font, 25)
+    rect(80, @height - 80, @footer.size * 10 + 100, 50)
+
+    fill(0)
+    text(@footer, 100, @height - 50)
+  end
+
+  def custom_post_draw
+
+  end
+
+end
--- examples/multi-presentation/start.rb
+++ examples/multi-presentation/start.rb
@@ -1,14 +1,11 @@
  require_relative '../../lib/soby'
 # require 'soby'
 
-# Give the current folder to Processing.
-# Processing::App::SKETCH_PATH = Dir.pwd
 
 Processing::App::SKETCH_PATH = __FILE__
 $:.unshift File.dirname(__FILE__)
 
 ## Presentation - relative elements
-
 
 Processing::PShapeSVG.TEXT_QUALITY = 2.0
 $app =  SobyPlayer.new 1920,1080
@@ -22,24 +19,3 @@ $app.presentation1 = presentation1
 $app.presentation2 = presentation2
 
 Soby::auto_update presentation1, __FILE__
-
-
-
-# #$app.background_min
-# $app.background_amplitude 5, 15
-# $app.background_min 10
-# $app.background_max 20
-# $app.background_rate 100
-# $app.color_mode false
-# $app.background_constrain = true
-
-# $app.background_amplitude 4, 5
-# $app.background_min 0
-# $app.background_max 100
-# $app.background_rate 1000
-# $app.color_mode true
-# $app.background_constrain = true
-
-  # $app.reset_background
-
-  # $app.background_constrain = false
